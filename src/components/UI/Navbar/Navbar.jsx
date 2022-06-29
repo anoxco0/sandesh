97463e2a74c805dc1a13db4085aa7e4141453d4f
Navneet Kumar <navneetharsh3@gmail.com>
2022-06-29 17:31:03 +0530

    searchbar ui completed

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -9,13 +9,13 @@ export const Navbar = () => {
         <img src="" alt="" />
       </div>
       <div className="con_nav_icons">
-        <svg viewBox="0 0 24 24" width="30px" class="">
+        <svg viewBox="0 0 24 24" width="30px" className="">
           <path
             fill="rgba(255, 255, 255, 0.6)"
             d="M19.005 3.175H4.674C3.642 3.175 3 3.789 3 4.821V21.02l3.544-3.514h12.461c1.033 0 2.064-1.06 2.064-2.093V4.821c-.001-1.032-1.032-1.646-2.064-1.646zm-4.989 9.869H7.041V11.1h6.975v1.944zm3-4H7.041V7.1h9.975v1.944z"
           ></path>
         </svg>
-        <svg viewBox="0 0 24 24" width="30px"class="">
+        <svg viewBox="0 0 24 24" width="30px"className="">
           <path
             fill="rgba(255, 255, 255, 0.6)"
             d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
