827563cf1b9ae249b2bdbb55b7171886b5c29bed
Navneet Kumar <navneetharsh3@gmail.com>
2022-06-30 13:50:31 +0530

    messege display ui done

--- src/components/UI/Messege/Messege.jsx
+++ src/components/UI/Messege/Messege.jsx
@@ -1,8 +1,13 @@
 import './messege.css';
-
+import {useSelector} from "react-redux"
+import image from './logo.png';
 export const Messege = ()=>{
-
+    const {theme} = useSelector(store=>store.settingReducer);
     return (
-        <div></div>
+        <div className='messege' style={{backgroundColor:theme[0]}}>
+           <img style={{width:"300px"}} src={image} alt="sandesh" />
+           <h1 style={{color:"white", fontSize:"80px"}}>Sandesh</h1>
+           <p style={{color:'white', fontSize:"20px"}}>Now you can join live chat.</p>
+        </div>
     )
 }
