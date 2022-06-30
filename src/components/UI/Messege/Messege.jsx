2fb6f70b30aea7079a6931331a2b95c3dad61165
Navneet Kumar <navneetharsh3@gmail.com>
2022-06-30 20:36:34 +0530

    messege body added

--- src/components/UI/Messege/Messege.jsx
+++ src/components/UI/Messege/Messege.jsx
@@ -1,8 +1,10 @@
 import './messege.css';
 import {useSelector} from "react-redux"
 // import image from './logo.png';
+// import imgBack from ".//messege_background.png"
 import { MessNav } from '../MessegeNav/MessNav';
 import { MessSearch } from '../MessSearch/MessSearch';
+import { MessegeBody } from '../MessegeBody/MessegeBody';
 export const Messege = ()=>{
     const {theme} = useSelector(store=>store.settingReducer);
     return (
@@ -13,6 +15,7 @@ export const Messege = ()=>{
         // </div>
         <div className='messeges' style={{backgroundColor:theme[2]}}>
             <MessNav/>
+            <MessegeBody/>
             <MessSearch/>
         </div>
     )
---
+++ src/components/UI/MessegeBody/MessegeBody.jsx
@@ -0,0 +1,12 @@
+import './messegebody.css';
+import imgback from './messege_background.png';
+export const MessegeBody = () =>{
+
+    return (
+        <div className='messege_body' style={{background:`url(${imgback})`, opacity:"0.07"}}>
+            <div className='messegeBody'>
+
+            </div>
+        </div>
+    )
+}
