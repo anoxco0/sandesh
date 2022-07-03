0b0a262aa638c996b8a3fc69ee0993ff3f3848a5
Navneet Kumar <navneetharsh3@gmail.com>
2022-07-04 02:29:12 +0530

    button set its own positions

--- src/components/UI/Contacts/Contacts.jsx
+++ src/components/UI/Contacts/Contacts.jsx
@@ -1,14 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { recieverUser } from '../../../redux/Messeges/action';
 import { AllContacts } from '../AllContacts/AllContacts';
 import { Navbar } from '../Navbar/Navbar';
 import { Search } from '../Searchbar/Search';
 import './contacts.css';
 
 export const Contacts = ()=>{
+    const {theme} = useSelector(store=>store.settingReducer);
+    const dispatch = useDispatch();
 
     return (
         <div className='contacts'>
             <Navbar/>
             <Search/>
+            <div style={{backgroundColor:theme[1], color:"white", padding:"10px 0", cursor:"pointer", borderBottom:"gray"}} onClick={()=>dispatch(recieverUser(null))}>Live chat...</div>
             <AllContacts/>
         </div>
     )
